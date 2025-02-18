52648f2ab63c397854d0ae3a4e5694f121f35e31
nasima-4755 <akter15-4755@diu.edu.bd>
2025-02-18 10:22:40 +0600

    Task

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,46 @@ void main() {
   runApp(const LabClass05());
 }
 
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Basic Flutter UI 02',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Basic Flutter"),
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+          ),
+          body: Row(
+            children: [
+              Icon(Icons.ac_unit,
+               size: 60, color: const Color.fromARGB(255, 0, 122, 129)),
+              Text(
+                "Hi,",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                "This is",
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontSize: 50,
+                ),
+              ),
+              Text(
+                "Flutter",
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 20,
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
